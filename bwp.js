cd6ad5c3e5ff01772258514d1bf7a5fdefc9c377
Flumonious9 <151790722+Flumonious9@users.noreply.github.com>
2025-03-23 16:29:01 -0700

    Add files via upload

---
+++ bwp.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const portfolio = document.querySelector(".portfolio");
+
+    portfolio.addEventListener('click', () => {
+        window.open('https://bwpportfolio.netlify.app/');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const research = document.querySelector(".research");
+
+    research.addEventListener('click', () => {
+        window.open('https://docs.google.com/document/d/18kaG9QBYetOTcKEeRJE8ETJF3oLK-_RAf_vZc4uLhnM/edit?usp=sharing');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const presentation = document.querySelector(".presentation");
+
+    presentation.addEventListener('click', () => {
+        window.open('https://www.canva.com/design/DAGYW-2n-GY/PeI9ZbcM5RyCcGz572C_FQ/edit?utm_content=DAGYW-2n-GY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const future_research = document.querySelector(".future_research");
+
+    future_research.addEventListener('click', () => {
+        window.open('https://futurebwp.netlify.app/');
+    });
+});
